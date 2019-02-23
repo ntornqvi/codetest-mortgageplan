6c2922f363cf296aa54f4fd10e0ebcaf2063aeaa
Nicklas <nicklas.tornqvist@ha.ax>
2019-02-23 16:05:14 +0200

    applikationen fungerar och några test har lagts till

--- mortgagecalculator/src/mortgagecalculator/Mortgagecalculator.java
+++ mortgagecalculator/src/mortgagecalculator/Mortgagecalculator.java
@@ -5,12 +5,19 @@
  */
 package mortgagecalculator;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -18,6 +25,8 @@ import javafx.stage.Stage;
  * @author nicke
  */
 public class Mortgagecalculator extends Application {
+    
+    private ArrayList<String> strings = new ArrayList();
     
     @Override
     public void start(Stage primaryStage) {
@@ -33,16 +42,51 @@ public class Mortgagecalculator extends Application {
             }
         });
         
+        VBox v = new VBox();
+        
+        Button showButton = new Button();
+        showButton.setText("Show Output");
+        showButton.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                
+                try {
+                    getStrings(parser);
+                } catch (IOException ex) {
+                    Logger.getLogger(Mortgagecalculator.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                for(String s : strings) {
+                    System.out.println(s);
+                    Text t = new Text(s);
+                    t.setWrappingWidth(250);
+                    v.getChildren().add(t);
+                }
+            }
+        });
+        
+        v.setSpacing(10);
+        v.setMinHeight(100);
+        
+        HBox h = new HBox();
+        h.getChildren().addAll(fileButton, showButton);
+        h.setSpacing(10);
+        
         GridPane root = new GridPane();
         
-        GridPane.setConstraints(fileButton, 0, 1);
-        root.getChildren().add(fileButton);
+        GridPane.setConstraints(v, 0, 0);
+        GridPane.setConstraints(h, 0, 1);
+        root.getChildren().addAll(v, h);
         
         Scene scene = new Scene(root, 300, 250);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    
+    public void getStrings(Parser parser) throws IOException {
+        this.strings = parser.getStrings();
     }
 
     /**
--- mortgagecalculator/src/mortgagecalculator/Parser.java
+++ mortgagecalculator/src/mortgagecalculator/Parser.java
@@ -5,7 +5,16 @@
  */
 package mortgagecalculator;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -15,6 +24,93 @@ import javafx.stage.Stage;
  */
 public class Parser {
     private File textFile;
+    
+    public Parser() {
+        
+    }
+    
+    public ArrayList<String> calculate(ArrayList<String> forCalc) {
+        for(String s : forCalc) {
+            if(0 > Double.parseDouble(s)) {
+                throw new IllegalArgumentException();
+            }
+        }
+        if(Double.parseDouble(forCalc.get(1)) > 100) {
+            throw new IllegalArgumentException();
+        }
+        double E;
+        double b;
+        double p;
+        double U = Double.parseDouble(forCalc.get(0));
+        double interest = Double.parseDouble(forCalc.get(1));
+        int years = Integer.parseInt(forCalc.get(2));
+        
+        p = years * 12;
+        
+        b = interest / 100 / 12;
+        
+        double temp1 = 1;
+        for(int x = 0; x < p; ++x) {
+            temp1 *= (1+b);
+        }
+                
+        E = U * (b * temp1) / (temp1 - 1);
+        
+        ArrayList<String> calculated = new ArrayList();
+        calculated.add(Double.toString(U));             //total loan
+        calculated.add(forCalc.get(2));                 //years
+        DecimalFormat df = new DecimalFormat("#.##");
+        calculated.add(df.format(E));                   //monthly payment
+        
+        return calculated;
+    }
+    
+    public String parse(String line, int x) {
+        ArrayList<String> matches = new ArrayList();
+        ArrayList<String> forCalc = new ArrayList();
+        Pattern regex = Pattern.compile("([^\"][^,]*|\".+?\")\\s*");
+        Matcher regexMatcher = regex.matcher(line);
+        while(regexMatcher.find()) {
+            matches.add(regexMatcher.group());
+        }
+        int y = 0;
+        for(String match : matches) {
+            match = match.replace(",", " ");
+            match = match.replace("\"", " ");
+            match = match.trim();
+            if(y != 0) {
+                forCalc.add(match);
+            }
+            ++y;
+        }
+        
+        ArrayList<String> calculated = new ArrayList();
+        
+        calculated = calculate(forCalc);
+        
+        String s = "Prospect" + " " + x + ": " + forCalc.get(0) + 
+                " wants to borrow " + calculated.get(0) + " for a period of " + 
+                calculated.get(1) + " years and pay " + calculated.get(2) +
+                " each month.";
+        
+        return s;
+    }
+    
+    public ArrayList<String> getStrings() throws FileNotFoundException, IOException {
+        ArrayList<String> strings = new ArrayList();
+        FileInputStream is = new FileInputStream(this.textFile);
+        InputStreamReader sr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(sr);
+        String line;
+        int x = 0;
+        while((line = br.readLine()) != null) {
+            if(x != 0 && line.trim().length() > 4) {
+                strings.add(parse(line, x));
+            }
+            ++x;
+        }
+        return strings;
+    }
     
     public void selectFile(Stage primaryStage) {
         FileChooser fileChooser = new FileChooser();
